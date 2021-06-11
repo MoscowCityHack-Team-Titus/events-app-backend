fc4c6ab578ab341a9eb93cca99a6ceafe19cda36
brshpl <igor_barishpol@mail.ru>
2021-06-12 00:16:22 +0300

    Добавлены обработчики основных запросов

---
+++ cmd/events-server/handlers.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+// TODO: разобраться, как идентифицировать пользователя (JWT, Cookies or smth)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// TODO: куча работы, связанной с показом релевантных событий
+
+	_, err := w.Write([]byte("Привет из Афиши"))
+	if err != nil {
+		return
+	}
+}
+
+func getEventInfo(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "Количество лайков, меток, всякой всячины для ивента %d...", id)
+	if err != nil {
+		return
+	}
+}
+
+func likeEvent(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод не дозволен", 405)
+		return
+	}
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// TODO: Проверяем в БД, лайкал ли этот пользователь это мероприятие - выставляем лайк, если нет
+
+	_, err = w.Write([]byte("Лайкнул событие? - Красавчик!"))
+	if err != nil {
+		return
+	}
+}
+
+func interactChat(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		// TODO: Постим сообщение в чятик
+
+		_, err = w.Write([]byte("Поболтал о событии? - Красавчик!"))
+		if err != nil {
+			return
+		}
+		return
+	} else if r.Method == http.MethodGet {
+		// TODO: выдаём последние сообщения в чатике
+
+		_, err = fmt.Fprintf(w, "Вот тебе ламповый чятик для ивента %d...", id)
+		if err != nil {
+			return
+		}
+	} else {
+		w.Header().Set("Allow", http.MethodPost) // TODO: разобраться, можно ли разрешить несколько методов
+		http.Error(w, "Метод не дозволен", 405)
+		return
+	}
+}
--- cmd/events-server/main.go
+++ cmd/events-server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/event", getEventInfo)
+	mux.HandleFunc("/like", likeEvent) // TODO: мейби объединить с '/event', но с методом POST?
+	mux.HandleFunc("/chat", interactChat)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
