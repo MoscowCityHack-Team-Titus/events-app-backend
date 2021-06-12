c474b0697581295b7f1bb94d5695c884e94c2c3c
brshpl <igor_barishpol@mail.ru>
2021-06-12 19:53:14 +0300

    Fixed paths in main.go

--- cmd/events-server/main.go
+++ cmd/events-server/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/MCHTitus/events-app-backend/config"
+	"github.com/MCHTitus/events-app-backend/internal/app/apiserver"
+	"github.com/MCHTitus/events-app-backend/internal/app/repository"
+	_ "github.com/MCHTitus/events-app-backend/migrations"
 	"github.com/spf13/viper"
-	"github.com/brshpl/events-app-backend/internal/app/apiserver"
-	"github.com/tetovske/events-app-backend/config"
-	"github.com/tetovske/events-app-backend/internal/app/repository"
-	_ "github.com/tetovske/events-app-backend/migrations"
 	"log"
-	"time"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -40,6 +40,6 @@ func main() {
 	mux.HandleFunc("/chat", apiserver.InteractChat)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
+	err = http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
